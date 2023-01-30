56fd07f3d65bd77cc4af90d5ac5e7e980b41b9f3
kaic-sa <kaictacker@gmai.com>
2023-01-30 13:22:50 -0300

    Rota do axios alteradas para IP fixo local

--- server/src/lib/route.ts
+++ server/src/lib/route.ts
@@ -124,7 +124,7 @@ export async function appRoute(app: FastifyInstance) {
           JOIN habits H
             ON H.id = HWD.habit_id
           WHERE
-            HWD.week_day = cast( strftime('%W', D.date/1000,'unixepoch') as int)
+            HWD.week_day = cast( strftime('%w', D.date/1000.0,'unixepoch') as int)
             AND H.created_at <= D.date
 
         )as amount
